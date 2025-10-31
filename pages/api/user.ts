98a894bbc8b15cb35203bbacb67fe2c2646bd813
JoseHiro <ipho.7johero@icloud.com>
2025-10-31 23:00:06 +0900

    delete log

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -37,8 +37,6 @@ export default async function handler(
     });
     if (!user) return res.status(404).json({ error: "User not found" });
 
-    // console.log(user);
-
     let trialStatus: "active" | "ended" | null = null;
     if (user.subscriptionStatus === "trialing") {
       const now = new Date();
