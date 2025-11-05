db3cf3a7e2dcecf696cdfc2cee513bcaf5336c20
Josey Sekine <108254240+JoseHiro@users.noreply.github.com>
2025-11-05 22:42:39 +0900

    Refactor Toaster component to use default theme

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner, ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme();
-
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
